c797f7f5bc79a5299f47115f555e689462504fd7
PKH Mateng <pkhmateng@gmail.com>
2025-11-05 11:15:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-345522778-caceb",
+  "appId": "1:861297320123:web:8f3720baa6d2cea7dac99e",
+  "apiKey": "AIzaSyAb0ZY_yLHNNJX9pkiMjph4Du19SwI5zbA",
+  "authDomain": "studio-345522778-caceb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "861297320123"
+};
