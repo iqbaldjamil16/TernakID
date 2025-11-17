4295b8608e21d97a6971f8c32038d8c021b01f21
PKH Mateng <pkhmateng@gmail.com>
2025-11-17 03:25:13 +0000

    <script type="module">   // Import the functions you need from the SDKs

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "e-ternak-48585649-334f0",
-  "appId": "1:528275860665:web:c0aa78d02a5075bfcfff68",
-  "apiKey": "AIzaSyBAS9f0Rw4eA7N-5IvPsUMa-illPnoo1_Q",
-  "authDomain": "e-ternak-48585649-334f0.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "528275860665"
-};
+    apiKey: "AIzaSyBAS9f0Rw4eA7N-5IvPsUMa-illPnoo1_Q",
+    authDomain: "e-ternak-48585649-334f0.firebaseapp.com",
+    projectId: "e-ternak-48585649-334f0",
+    storageBucket: "e-ternak-48585649-334f0.firebasestorage.app",
+    messagingSenderId: "528275860665",
+    appId: "1:528275860665:web:a039c8c39bcff92bcfff68"
+  };
