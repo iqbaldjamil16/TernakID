be6d500a984861c5cb454be1a5324d6983a74bfd
PKH Mateng <pkhmateng@gmail.com>
2025-11-20 09:07:38 +0000

    hapus data KIT-016 hingga KIT-100 pada aplikasi ini

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -83,7 +83,7 @@ export async function createDefaultAnimals(count = 14) {
 
 export function listenToAnimals(callback: (animals: Livestock[]) => void): () => void {
   const { firestore } = initializeFirebase();
-  const livestockCollectionRef = collection(firestore, LIVESTOCK_COLLECTION);
+  const livestockCollectionRef = collection(firestore, LIVESTOCK_COLlection);
   const unsubscribe = onSnapshot(livestockCollectionRef, (snapshot) => {
     const animals = snapshot.docs.map(doc => {
         const data = doc.data();
