bdee40ed09433248b7826c8bf5d6bfc24991029b
PKH Mateng <pkhmateng@gmail.com>
2025-11-20 02:41:22 +0000

    adakah cara lain agar aplikasi ini dapat di publish atau dihosting

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
     apiKey: "AIzaSyBAS9f0Rw4eA7N-5IvPsUMa-illPnoo1_Q",
     authDomain: "e-ternak-48585649-334f0.firebaseapp.com",
     projectId: "e-ternak-48585649-334f0",
-    storageBucket: "e-ternak-48585649-334f0.firebasestorage.app",
+    storageBucket: "e-ternak-48585649-334f0.appspot.com",
     messagingSenderId: "528275860665",
     appId: "1:528275860665:web:a039c8c39bcff92bcfff68"
   };
